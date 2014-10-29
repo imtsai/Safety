a7b5f079da0bdb646d2f4dce4c47fc57bcf676f8
Ryan Ma <ryan.ma11@yahoo.com>
2014-10-28 22:34:44 -0700

    Implemented getpointsfromserver

--- src/com/example/safety/display_routes.java
+++ src/com/example/safety/display_routes.java
@@ -7,6 +7,7 @@ import android.webkit.WebView;
 
 public class display_routes extends Activity {
 	private WebView webView;
+	private String resultstring;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -16,8 +17,33 @@ public class display_routes extends Activity {
 	    setContentView(R.layout.display_routes);
 	    webView = (WebView) findViewById(R.id.webView);
 		webView.getSettings().setJavaScriptEnabled(true);
-		webView.loadUrl("https://goo.gl/maps/8CegB");
- 
+		resultstring = "www.google.com/maps/dir/" + getPointsFromServer();
+		webView.loadUrl(resultstring);
 	}
+
+	public String getPointsFromServer(){
+		HttpEntity resultentity = httpresponse.getEntity();
+		InputStream inputstream = resultentity.getContent();
+		Header contentencoding = httpresponse.getFirstHeader("Content-Encoding");
+		if(contentencoding != null && contentencoding.getValue().equalsIgnoreCase("gzip")) 
+			inputstream = new GZIPInputStream(inputstream);
+		resultstring = convertStreamToString(inputstream);
+		inputstream.close();
+		return resultstring;
+	}
+
+	private String convertStreamToString(InputStream input){
+		String line = "";
+		StringBuilder total = new StringBuilder();
+		BufferedReader rd = new BufferedReader(new InputStream(input));
+		try {
+			while ((line = rd.readline() != null)
+				total.append(line + "/"))
+		} catch (Exception e){
+			Toast.makeText(this, "Stream Exception", Toast.LENGTH_SHORT).show();
+		}
+		return total.toString();
+	}
+
 
 }
