f0f68b3ebcb44695859ceae9a99d6e85afbaf84a
Fangzhou Chen <kevinchen1219@gmail.com>
2014-10-30 14:37:19 -0700

    Revert "Merge branch 'master' of https://github.com/imtsai/Safety"
    This reverts commit febd31b64a3fddb4172e6bfe8647a0667df8b99c, reversing
    changes made to af6aa65ae31b88d352d462e645c436c83282146c.

--- src/com/example/safety/routes_results.java
+++
@@ -1,7 +0,0 @@
-package com.example.safety;
-//todo: copy paste enter_location.java and relate stuff that relate to routes_results in enter_location
-//this is required for us to use the BACK BUTTON OR ELSE THINGS WILL BREAK
-//-hubert
-public class routes_results {
-
-}
